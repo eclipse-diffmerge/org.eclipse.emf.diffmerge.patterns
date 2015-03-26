021c32150037a9af5899d51b583be0a4615184c9
Olivier Constant <olivier.constant@thalesgroup.com>
2015-03-26 14:08:20 +0100

    Bug 463181 - Make the pattern/instance sync mechanism tolerant to duplicates
    Change-Id: Ie216f9725e38bea6c0d470a97a37fa55cc7ce969
    Signed-off-by: Olivier Constant <olivier.constant@thalesgroup.com>

--- core/plugins/org.eclipse.emf.diffmerge.patterns.templates.engine/src/org/eclipse/emf/diffmerge/patterns/templates/engine/diffmerge/TemplatePatternContentScope.java
+++ core/plugins/org.eclipse.emf.diffmerge.patterns.templates.engine/src/org/eclipse/emf/diffmerge/patterns/templates/engine/diffmerge/TemplatePatternContentScope.java
@@ -14,8 +14,15 @@
  */
 package org.eclipse.emf.diffmerge.patterns.templates.engine.diffmerge;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.emf.diffmerge.impl.scopes.RootedModelScope;
 import org.eclipse.emf.diffmerge.patterns.templates.gen.templatepatterns.TemplatePattern;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 
 /**
@@ -36,6 +43,62 @@ public class TemplatePatternContentScope extends RootedModelScope {
   public TemplatePatternContentScope(TemplatePattern pattern_p) {
     super(pattern_p.getTemplateElements(), true);
     _pattern = pattern_p;
+  }
+  
+  /**
+   * @see org.eclipse.emf.diffmerge.impl.scopes.AbstractModelScope#get(org.eclipse.emf.ecore.EObject, org.eclipse.emf.ecore.EReference, boolean)
+   */
+  @Override
+  protected List<EObject> get(EObject source_p, EReference reference_p,
+      boolean resolveProxies_p) {
+    List<EObject> defaultResult = super.get(source_p, reference_p, resolveProxies_p);
+    // Handling the exceptional case where the pattern catalog contains duplicate IDs
+    List<EObject> result = new ArrayList<EObject>(defaultResult.size());
+    TemplatePattern expectedPattern = getPattern();
+    for (EObject value : defaultResult) {
+      TemplatePattern valuePattern = getContainingPattern(value);
+      if (valuePattern != null && valuePattern != expectedPattern) {
+        EObject expectedElement = getExpectedElement(value, expectedPattern);
+        if (expectedElement != null)
+          value = expectedElement;
+      }
+      result.add(value);
+    }
+    return result;
+  }
+  
+  /**
+   * Return the pattern that contains the given element, if any
+   * @param element_p a non-null element
+   * @return a potentially null object
+   */
+  protected TemplatePattern getContainingPattern(EObject element_p) {
+    EObject container = element_p.eContainer();
+    while (container != null && !(container instanceof TemplatePattern)) {
+      container = container.eContainer();
+    }
+    TemplatePattern result = container instanceof TemplatePattern? (TemplatePattern)container: null;
+    return result;
+  }
+  
+  /**
+   * Return the element that corresponds to the given one within the given pattern, if any
+   * @param element_p a non-null element
+   * @param expectedPattern_p a non-null pattern
+   * @return a potentially null element
+   */
+  protected EObject getExpectedElement(EObject element_p, TemplatePattern expectedPattern_p) {
+    String id = EcoreUtil.getID(element_p);
+    if (id != null) {
+      Iterator<EObject> it = expectedPattern_p.eAllContents();
+      while (it.hasNext()) {
+        EObject current = it.next();
+        String currentId = EcoreUtil.getID(current);
+        if (id.equals(currentId))
+          return current;
+      }
+    }
+    return null;
   }
   
   /**
